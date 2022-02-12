bca3e91e9e8384d6452e198e0c728381734b01c0
Venkatamanisha <95544482+Venkatamanisha@users.noreply.github.com>
2022-02-12 11:30:09 +0530

    semantic-ui design

--- index.js
+++ index.js
@@ -1,38 +1,70 @@
 import React from 'react';
 import  ReactDOM from 'react-dom';
-import "./style.css";
-import image from './images/lizard.png';
-function Student1(){
+import Image from 'faker'
+import style from './style.css';
+
+
+function Faker1 (props){
     return(
-   
-<div  >
-    <br/>
-    <br/>
-    <br/>
-<img src={image} height={400} width={500} />
-<br>
-</br>
-<h2>Lizard</h2>
-<p>
-    Lizards are dangerous and poisionous .Many of the lizards are green in color .It was observed in forest.Lizards are widespread group of squamate reptiles,with over 60,000.
-</p>
-
-   <a href='#'>Share</a> &nbsp;&nbsp;
-   <a href='#'>Learn More</a> 
-   </div>
-
-    )
         
+        <div class="ui cards">
+            
+            <div class='card' >
+            <div class="content">
+                <img class="right floated mini ui image" src= {Image.image.image()} alt=""/>
+                
+                <div class="header">
+                    Elliot Fu
+                </div>
+                <div class="meta">
+                    Friends of Veronika
+                </div>
+                <div class="description">Elliot requested permission to view your content
+            </div>
+        </div>
+        <div className='extra content'>
+            <div className='ui two buttons'>
+                <div className='ui basic green button'>{props.accept?"yes":"No"}</div>
+                <div className='ui basic red button'>Decline</div>
+            </div>
+        </div>
+        </div>,
         
-      
+        <div class="ui cards">
+            
+        <div class='card'>
+        <div class="content">
+            <img class="right floated mini ui image" src= {Image.image.image()} alt=""/>
+            
+            <div class="header">
+Jenny Hess
+            </div>
+            <div class="meta">
+                New Member
+            </div>
+            <div class="description">Jenny wants to add you to the group best friends
+        </div>
+    </div>
     
+    <div className='extra content'>
+        <div className='ui two buttons'>
+            <div className='ui basic green button'>{props.decline?"Yes":"No"}</div>
+            <div className='ui basic red button'>Decline</div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+        
+    )
 }
-
 
 ReactDOM.render(
     <>
-       <Student1/>
        
+       <Faker1  name="manisha" accept={true} decline={false}/>
+    
     </>,
     
     document.getElementById('root')
